c07f114b4a2692fe707da21ee99da78b884794ab
Paolo Dávila <pdavila@iesebre.com>
2017-05-23 15:59:46 +0200

    Added documentation for Controller

--- src/Http/Controllers/Controller.php
+++ src/Http/Controllers/Controller.php
@@ -7,6 +7,11 @@ use Illuminate\Routing\Controller as BaseController;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 
+/**
+ * Class Controller
+ *
+ * @package Scool\Curriculum\Http\Controllers
+ */
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
