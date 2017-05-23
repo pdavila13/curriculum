17ff7a10e14aead428c6b359d13c9f42933b3e62
Paolo Dávila <pdavila@iesebre.com>
2017-05-23 09:57:32 +0200

    Added and implement course table

---
+++ database/migrations/2017_05_23_075113_create_courses_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCoursesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('courses', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name')->unique();
+            $table->timestamps();
+        });
+
+        Schema::create('course_study', function (Blueprint $table) {
+            $table->integer('course_id')->unsigned();
+            $table->integer('study_id')->unsigned();
+            $table->timestamps();
+            $table->unique(['course_id', 'study_id']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('courses');
+        Schema::dropIfExists('course_study');
+    }
+}
